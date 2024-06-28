4b80b209db1e3fa6b07b35955e06b319db1d107f
shubhamjangid-dev <s.jangid1100@gmail.com>
2024-06-28 21:05:39 +0530

    update

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,6 +1,6 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   let [length, setLength] = useState(8);
@@ -11,54 +11,88 @@ function App() {
 
   // useRef hook
   const passwordRef = useRef(password);
-  const copyToClipBoard = useCallback(()=>{
+  const copyToClipBoard = useCallback(() => {
     passwordRef.current?.select();
     // passwordRef.current?.setSelectionRange(0,3);
-    window.navigator.clipboard.writeText(password)
-    setCopy("copied")
-  }, [password])
+    window.navigator.clipboard.writeText(password);
+    setCopy("copied");
+  }, [password]);
 
-
-  const passwordGenerator = useCallback(()=>{
+  const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
-    if(numberAllowed) str +="0123456789";
-    if(characterAllowed) str +="~`!@#$%^&*()_-+=?"
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "~`!@#$%^&*()_-+=?";
 
-    for(let i = 0;i<length;i++)
-    {
-      let index = Math.floor(Math.random()*str.length)
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * str.length);
       pass += str[index];
     }
 
-    setPassword(pass)
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
 
-  },[length, numberAllowed, characterAllowed, setPassword]);
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
 
-  useEffect(()=>{
-    passwordGenerator()
-  }, [length, numberAllowed, characterAllowed, passwordGenerator])
-
-  useEffect(()=>{
-    setCopy("copy")
-  }, [length, numberAllowed, characterAllowed, passwordGenerator])
+  useEffect(() => {
+    setCopy("copy");
+  }, [length, numberAllowed, characterAllowed, passwordGenerator]);
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-red-200" >
-        <h1 className = "text-3xl my-2">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-red-200">
+        <h1 className="text-3xl my-2">Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input type="text" className = "outline-none w-full py-1 px-2.5 font-mono text-gray-500" value={password} readOnly ref = {passwordRef}/>
-          <button onClick={copyToClipBoard} className = "outline-none bg-blue-500 text-white px-3 py-1 shrink-0">{copy}</button>
+          <input
+            type="text"
+            className="outline-none w-full py-1 px-2.5 font-mono text-gray-500"
+            value={password}
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyToClipBoard}
+            className="outline-none bg-blue-500 text-white px-3 py-1 shrink-0"
+          >
+            {copy}
+          </button>
         </div>
-        <div className='flex text-sm gap-x-2'>
-          <input type="range" className='' min={8} max={50} value={length} onChange={(e)=>{setLength(e.target.value)}}/> length = {length}
-          <input type="checkbox" className = "" value={numberAllowed} onChange={(prev)=>{setNumberAllowed((prev)=>!prev)}}/>Numbers
-          <input type="checkbox" className = "" value={characterAllowed} onChange={(prev)=>{setCharacterAllowed((prev)=>!prev)}}/>characters
+        <div className="flex text-sm gap-x-2">
+          <input
+            type="range"
+            className=""
+            min={8}
+            max={50}
+            value={length}
+            onChange={e => {
+              setLength(e.target.value);
+            }}
+          />{" "}
+          length = {length}
+          <input
+            type="checkbox"
+            className=""
+            value={numberAllowed}
+            onChange={prev => {
+              setNumberAllowed(prev => !prev);
+            }}
+          />
+          Numbers
+          <input
+            type="checkbox"
+            className=""
+            value={characterAllowed}
+            onChange={prev => {
+              setCharacterAllowed(prev => !prev);
+            }}
+          />
+          characters
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
